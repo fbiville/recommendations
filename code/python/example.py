2c65b9f812b959648ef04293813fdfeb385cd18e
Florent Biville <florent.biville@neo4j.com>
2021-03-25 17:56:54 +0100

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/f5f46a7b733d1e726558bd3654b37e844eff7038

--- code/python/example.py
+++ code/python/example.py
@@ -4,18 +4,19 @@
 from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>",
+  "bolt://<HOST>:<BOLTPORT>", 
   auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
-MATCH (m:Movie {title:$movie})<-[:RATED]-(u:User)-[:RATED]->(rec:Movie) 
+MATCH (m:Movie {title:$movie})<-[:RATED]-(u:User)-[:RATED]->(rec:Movie)
 RETURN distinct rec.title AS recommendation LIMIT 20
 '''
 
 with driver.session(database="neo4j") as session:
   results = session.read_transaction(
     lambda tx: tx.run(cypher_query,
-                      movie="Crimson Tide").data())
+      movie="Crimson Tide").data())
+
   for record in results:
     print(record['recommendation'])
 
